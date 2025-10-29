d21524df8f223fbc72ffe845bfb660880d57d488
ilk2 <ilk2@illinois.edu>
2025-10-29 11:59:39 -0500

    Modified script to draw a rectangle and a line between two points

--- Demo/Demo.py
+++ Demo/Demo.py
@@ -16,35 +16,19 @@ def run(_context: str):
         
         design = adsk.fusion.Design.cast(app.activeProduct)
         
-        # Prompt the user for a string and validate it's valid.
-        isValid = False
-        input = '1 in'  # The initial default value.
+        rootComp = design.rootComponent
         
-        while not isValid:
-            # Get a string from the user.
-            retVals = ui.inputBox('Enter a distance', 'Distance', input)
-            if retVals[0]:
-                (input, isCancelled) = retVals
-            
-            # Exit the program if the dialog was cancelled.
-            if isCancelled:
-                return
-            
-            # Check that a valid length description was entered.
-            unitsMgr = design.unitsManager
-            try:
-                realValue = unitsMgr.evaluateExpression(input, unitsMgr.defaultLengthUnits)
-                isValid = True
-            except:
-                # Invalid expression so display an error and set the flag to allow them
-                # to enter a value again.
-                ui.messageBox('"' + input + '" is not a valid length expression.', 'Invalid entry', 
-                              adsk.core.MessageBoxButtonTypes.OKButtonType, 
-                              adsk.core.MessageBoxIconTypes.CriticalIconType)
-                isValid = False
+        sketches = rootComp.sketches
+        xyPlane = rootComp.xYConstructionPlane
         
-        # Use the value for something.
-        ui.messageBox('input: ' + input + ', result: ' + str(realValue))
+        sketch = sketches.add(xyPlane)
+        
+        sketchLines = sketch.sketchCurves.sketchLines
+        startPoint = adsk.core.Point3D.create(0, 0, 0)
+        endPoint = adsk.core.Point3D.create(5, 5, 0)
+
+        sketchLines.addCenterPointRectangle(startPoint, endPoint)
+        sketchLines.addByTwoPoints(startPoint, endPoint)
 
     except:  #pylint:disable=bare-except
         # Write the error message to the TEXT COMMANDS window.
