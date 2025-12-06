30e152aca457a25cb99a7a82e9679dbd7e4b485e
ilk2 <ilk2@illinois.edu>
2025-12-06 12:29:31 -0600

    refactored code

--- CircularResonantCavity/CircularResonantCavity.py
+++ CircularResonantCavity/CircularResonantCavity.py
@@ -13,9 +13,7 @@ ui  = app.userInterface
 
 # User input helpers
 
-def get_param_length(design: adsk.fusion.Design, prompt: str, title: str, default: str) -> None | float:
-
-    units_manager = design.unitsManager
+def get_param_length(units_manager: adsk.core.UnitsManager, prompt: str, title: str, default: str) ->  None | float:
 
     while True:
 
@@ -57,10 +55,12 @@ def get_param_quantity(prompt: str, title: str, default: str) -> None | int:
 
 # Sketch helpers
 
-def reflect_point(pt, x=False, y=False, z=False):
+def reflect_point(points: adsk.fusion.SketchPoints, orig_pt: adsk.core.Point3D, x: bool=False, y: bool=False, z:bool=False) -> adsk.fusion.SketchPoint:
 
-    new_pt = adsk.core.Point3D.create((-1) ** x * pt.x, (-1) ** y * pt.y, (-1) ** z * pt.z)
-    return new_pt
+    new_pt = adsk.core.Point3D.create((-1) ** x * orig_pt.x, (-1) ** y * orig_pt.y, (-1) ** z * orig_pt.z)
+    new_sk = points.add(new_pt)
+
+    return new_sk
 
 def rotate_entities(geom_cons, center_sk_pt, entities, quantity):
 
@@ -85,6 +85,98 @@ def rotate_point(pt: adsk.core.Point3D, ang_deg: float, center: adsk.core.Point3
 
     return adsk.core.Point3D.create(x_new, y_new, pt.z)
 
+# Geometry helpers
+
+def get_circle_circum(circle: adsk.fusion.SketchCircle) -> float:
+
+    r = circle.radius
+    return 2 * math.pi * r
+
+def get_arc_length_theta(arc_length: float, radius: float) -> float:
+
+    theta = arc_length * 360 / (2 * math.pi * radius)
+    return theta
+
+# Script helpers
+
+def get_params(units_manager: adsk.core.UnitsManager) -> list[float | int]:
+
+    # Get electrode radius
+
+    r = get_param_length(units_manager, "Enter the electrode radius r:", "Electrode Radius", "12.5 mm")
+
+    # if r is None:
+    #     exit(0)
+    
+    # Get shield radius
+
+    R = get_param_length(units_manager, "Enter the shield radius R:", "Shield Radius", "18 mm")
+
+    # if R is None:
+    #     exit(0)
+    
+    # Get electrode width
+
+    w = get_param_length(units_manager, "Enter the electrode width w:", "Electrode Width", "3.8 mm")
+
+    # if w is None:
+    #     exit(0)
+
+    # Get shield width
+
+    W = get_param_length(units_manager, "Enter the shield width W:", "Shield Width", "5 mm")
+
+    # if W is None:
+    #     exit(0)
+
+    # Get electrode height
+
+    h = get_param_length(units_manager, "Enter the electrode height h:", "Electrode Height", "20 mm")
+
+    # if h is None:
+    #     exit(0)
+
+    # Get shield height
+
+    H = get_param_length(units_manager, "Enter the shield height H:", "Shield Height", "22.5 mm")
+
+    # if H is None:
+    #     exit(0)
+
+    # Get gap length
+
+    t = get_param_length(units_manager, "Enter the gap length t:", "Gap Length", "2.138 mm")
+
+    if H is None:
+        exit(0)
+
+    # Get gap quantity
+
+    n = get_param_quantity("Enter the quantity of gaps n:", "Gap Quantity", "6")
+
+    # if n is None:
+    #     exit(0)
+
+    return [r, R, w, W, h, H, t, n]
+
+def draw_circle(circles: adsk.fusion.SketchCircles, center: adsk.core.Point3D, radius: float) -> tuple[adsk.fusion.SketchCircle, adsk.fusion.SketchPoint]:
+
+    circle = circles.addByCenterRadius(center, radius)
+
+    # Fix circle at its center
+    circle_center = circle.centerSketchPoint
+    circle_center.isFixed = True
+
+    return circle, circle_center
+
+def extrude_profiles(extrudes: adsk.fusion.ExtrudeFeatures, profiles: adsk.core.ObjectCollection, distance: float, extrusion_is_sym: bool=True, extrusion_op: adsk.fusion.ExtrudeFeatureInput=adsk.fusion.FeatureOperations.NewBodyFeatureOperation):
+
+    dist = adsk.core.ValueInput.createByReal(distance)
+
+    extrusion = extrudes.createInput(profiles, adsk.fusion.FeatureOperations.NewBodyFeatureOperation)
+    extrusion.setDistanceExtent(extrusion_is_sym, dist)
+    extrudes.add(extrusion)
+
 # Main
 
 def run(_context: str):
@@ -95,8 +187,9 @@ def run(_context: str):
         
         design = adsk.fusion.Design.cast(app.activeProduct)
         root_comp = design.rootComponent
+        units_manager = design.unitsManager
 
-        extrudes = root_comp.features.extrudeFeatures
+        # Sketches
 
         sketches = root_comp.sketches
         xy_plane = root_comp.xYConstructionPlane
@@ -109,111 +202,68 @@ def run(_context: str):
         dims = sketch.sketchDimensions
         geom_cons = sketch.geometricConstraints
 
+        # Bodies
+
+        extrudes = root_comp.features.extrudeFeatures
+
+        # Get Params
+
+        [r, R, w, W, h, H, t, n] = get_params(units_manager)
+        
+        # Create model 
+
         center = adsk.core.Point3D.create(0, 0, 0)
-        center_sk_pt = points.add(center)
+        center_sk = points.add(center)
 
-        # Get electrode inner circle radius
-
-        r = get_param_length(design, "Enter the electrode inner circle radius r:", "Electrode Inner Circle Radius", "12.5 mm")
+        # Draw circles and get properties
         
-        if r is None:
-            return
+        c1, c1_center_sk = draw_circle(circles, center_sk, r)
+        c2, _ = draw_circle(circles, center_sk, R)
 
-        # Get shield radius
+        c3, c3_center_sk = draw_circle(circles, center_sk, r + w)
+        draw_circle(circles, center_sk, R + W)
 
-        R = get_param_length(design, "Enter the shield radius R:", "Shield Radius", "18 mm")
-        
-        if R is None:
-            return
+        c3_circum = get_circle_circum(c3)
 
-        # Get electrode width
+        # Draw gap line
 
-        w = get_param_length(design, "Enter the electrode width w:", "Electrode Width", "3.8 mm")
+        c1_pt = adsk.core.Point3D.create(c1.radius, 0, 0)
+        c1_sk = points.add(c1_pt)
 
-        if w is None:
-            return
+        c3_pt = adsk.core.Point3D.create(c3.radius, 0, 0)
+        c3_sk = points.add(c3_pt)
 
-        # Get shield width
-
-        W = get_param_length(design, "Enter the shield width W:", "Shield Width", "5 mm")
-
-        if W is None:
-            return
-
-        # Get gap length
-
-        t = get_param_length(design, "Enter the gap length t:", "Gap Length", "2.138 mm")
-
-        if t is None:
-            return
-
-        # Get gap quantity
-
-        n = get_param_quantity("Enter the quantity of gaps n:", "Gap Quantity", "6")
-
-        if n is None:
-            return
-
-        # Create model based on constraints
-        
-        c1 = circles.addByCenterRadius(center, r)
-        c1_center_pt = c1.centerSketchPoint
-        c1_center_pt.isFixed = True
-
-        c2 = circles.addByCenterRadius(center, R)
-        c2_center_pt = c2.centerSketchPoint
-        c2_center_pt.isFixed = True
-
-        c3 = circles.addByCenterRadius(center, r + w) # + offset w
-        c3_circum = 2 * math.pi * (r + w)
-
-        c3_center_pt = c3.centerSketchPoint
-        c3_center_pt.isFixed = True 
-
-        c4 = circles.addByCenterRadius(center, R + W) # + offset w
-        c4_center_pt = c4.centerSketchPoint
-        c4_center_pt.isFixed = True 
-
-        # Add guess pts
-
-        c1_guess = adsk.core.Point3D.create(r, 0, 0)
-        c1_sk_pt = points.add(c1_guess)
-
-        c3_guess = adsk.core.Point3D.create(r + w, 0, 0)
-        c3_sk_pt = points.add(c3_guess)
-
-        # Add coincident constraint to guess pts
-
-        sketch.geometricConstraints.addCoincident(c1_sk_pt, c1)
-        sketch.geometricConstraints.addCoincident(c3_sk_pt, c3)
+        sketch.geometricConstraints.addCoincident(c1_sk, c1)
+        sketch.geometricConstraints.addCoincident(c3_sk, c3)
 
         c1_text_pt = adsk.core.Point3D.create(t, 0, 0)
         c3_text_pt = adsk.core.Point3D.create(t, 0, 0)
 
-        # Set gap pts
+        dims.addDistanceDimension(c1_center_sk, c1_sk, adsk.fusion.DimensionOrientations.VerticalDimensionOrientation, c1_text_pt).parameter.value = t / 2
+        dims.addDistanceDimension(c3_center_sk, c3_sk, adsk.fusion.DimensionOrientations.VerticalDimensionOrientation, c3_text_pt).parameter.value = t / 2
 
-        dims.addDistanceDimension(c1_center_pt, c1_sk_pt, adsk.fusion.DimensionOrientations.VerticalDimensionOrientation, c1_text_pt).parameter.value = t / 2
-        dims.addDistanceDimension(c3_center_pt, c3_sk_pt, adsk.fusion.DimensionOrientations.VerticalDimensionOrientation, c3_text_pt).parameter.value = t / 2
-
-        # Add line between gap pts
-
-        l1 = lines.addByTwoPoints(c1_sk_pt, c3_sk_pt)
+        l1 = lines.addByTwoPoints(c1_sk, c3_sk)
         l1_length = l1.length
 
-        # mirror line
+        # Mirror gap line
 
-        rc1_sk_pt = reflect_point(c1_sk_pt.geometry, y=True)
-        rc3_sk_pt = reflect_point(c3_sk_pt.geometry, y=True)
-        l2 = lines.addByTwoPoints(rc1_sk_pt, rc3_sk_pt)
+        rc1_sk = reflect_point(points, c1_sk.geometry, y=True)
+        rc3_sk = reflect_point(points, c3_sk.geometry, y=True)
+        
+        l2 = lines.addByTwoPoints(rc1_sk, rc3_sk)
 
-        # rotate lines
+        # Rotate lines n times
 
-        rotate_entities(geom_cons, center_sk_pt, [l1, l2], n)
+        rotate_entities(geom_cons, center_sk, [l1, l2], n)
 
-        # Find segment
+        # Draw spruces
 
         gap_area = t * l1_length
-        segment_deg = ((c3_circum - n * l1_length) / (2 * math.pi * (r + w)) * 360) / n * 0.25
+
+        approx_gap_arc_lengths = n * l1_length
+        approx_segment_arc_lengths = c3_circum - approx_gap_arc_lengths
+
+        spruce_half_angle = (get_arc_length_theta(approx_segment_arc_lengths, c3.radius) / n) * 0.25
 
         for profile in sketch.profiles:
 
@@ -227,28 +277,30 @@ def run(_context: str):
 
             if (gap_area < area) and (r < radius and radius < r + w):
                 
-                pos_rot1 = rotate_point(centroid, int(segment_deg), center)
+                pos_rot1 = rotate_point(centroid, int(spruce_half_angle), center)
                 pos_rot2 = pos_rot1
 
-                neg_rot1 = rotate_point(centroid, -int(segment_deg), center)
-                neg_rot2 = neg_rot1
-                
                 pos_rot1_sk_pt = points.add(pos_rot1)
                 pos_rot2_sk_pt = points.add(pos_rot2)
 
-                sketch.geometricConstraints.addCoincident(pos_rot1_sk_pt, c2)
-                sketch.geometricConstraints.addCoincident(pos_rot2_sk_pt, c3)
+                geom_cons.addCoincident(pos_rot1_sk_pt, c2)
+                geom_cons.addCoincident(pos_rot2_sk_pt, c3)
+
+                neg_rot1 = rotate_point(centroid, -int(spruce_half_angle), center)
+                neg_rot2 = neg_rot1
 
                 neg_rot1_sk_pt = points.add(neg_rot1)
                 neg_rot2_sk_pt = points.add(neg_rot2)
 
-                sketch.geometricConstraints.addCoincident(neg_rot1_sk_pt, c2)
-                sketch.geometricConstraints.addCoincident(neg_rot2_sk_pt, c3)
+                geom_cons.addCoincident(neg_rot1_sk_pt, c2)
+                geom_cons.addCoincident(neg_rot2_sk_pt, c3)
 
                 l3 = lines.addByTwoPoints(pos_rot1_sk_pt, pos_rot2_sk_pt)
                 l4 = lines.addByTwoPoints(neg_rot1_sk_pt, neg_rot2_sk_pt)
 
-                rotate_entities(geom_cons, center_sk_pt, [l3, l4], n)
+                rotate_entities(geom_cons, center_sk, [l3, l4], n)
+
+        # Get spruce area
 
         min_area = float("inf")
         for profile in sketch.profiles:
@@ -262,23 +314,11 @@ def run(_context: str):
             radius = math.sqrt(x * x + y * y)
 
             if (r + w < radius and radius < R):
-                # ui.messageBox(area)
                 min_area = min(min_area, area)
 
+        # Extrude spruce and electrode
+
         profile_collection = adsk.core.ObjectCollection.create()
-        
-        for profile in sketch.profiles:
-
-            props = profile.areaProperties()
-
-            area = props.area
-            centroid = props.centroid
-
-            x, y = centroid.x, centroid.y
-            radius = math.sqrt(x * x + y * y)
-
-            if (gap_area < area) and (r < radius and radius < r + w):
-                profile_collection.add(profile)
 
         for profile in sketch.profiles:
 
@@ -290,43 +330,32 @@ def run(_context: str):
             x, y = centroid.x, centroid.y
             radius = math.sqrt(x * x + y * y)
 
-            if abs(area - (1 + 1e-5) * min_area) <= 1e-5 and (r + w < radius and radius < R):
+            if (gap_area < area) and (r < radius and radius < r + w):
                 profile_collection.add(profile)
 
-        distance = adsk.core.ValueInput.createByReal(2)
+            if abs(area - (1 + 1e-5) * min_area) <= 1e-5 and (r + w < radius and radius < R):
+                profile_collection.add(profile)
         
-        ext_input = extrudes.createInput(profile_collection, adsk.fusion.FeatureOperations.NewBodyFeatureOperation)
-        ext_input.setDistanceExtent(True, distance)
+        extrude_profiles(extrudes, profile_collection, h)
 
-        extrudes.add(ext_input)
+        # Extrude shield
 
-        distance = adsk.core.ValueInput.createByReal(2.5)
-
-        wanted_profile = None
-        max_area = 0
-
+        shield_profile = None
+        max_diag_len = 0
         for profile in sketch.profiles:
 
-            props = profile.areaProperties()
+            bb = profile.boundingBox
+            diag_vec = bb.minPoint.vectorTo(bb.maxPoint)
+            diag_len = diag_vec.length 
 
-            area = props.area
-            centroid = props.centroid
-
-            x, y = centroid.x, centroid.y   
-
-            if abs(x) <= 1e-5 and abs(y) <= 1e-5 and max_area < area:
-                wanted_profile = profile
-                max_area = area
-
-        distance = adsk.core.ValueInput.createByReal(2.25)
+            if max_diag_len < diag_len:
+                shield_profile = profile
+                max_diag_len = diag_len
         
         profile_collection = adsk.core.ObjectCollection.create()
-        profile_collection.add(wanted_profile)
+        profile_collection.add(shield_profile)
 
-        ext_input = extrudes.createInput(profile_collection, adsk.fusion.FeatureOperations.JoinFeatureOperation)
-        ext_input.setDistanceExtent(True, distance)
-
-        extrudes.add(ext_input)
+        extrude_profiles(extrudes, profile_collection, H)
         
     except:  #pylint:disable=bare-except
         # Write the error message to the TEXT COMMANDS window.
