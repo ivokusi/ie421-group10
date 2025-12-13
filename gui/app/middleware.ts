07f0b090f5876037f5187b4efc86c9522d2ff235
ilk2 <ilk2@illinois.edu>
2025-12-13 17:27:07 -0600

    added middleware.ts

---
+++ gui/app/middleware.ts
@@ -0,0 +1,8 @@
+export const runtime = 'edge';
+
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(_req: NextRequest) {
+  return NextResponse.next();
+}
